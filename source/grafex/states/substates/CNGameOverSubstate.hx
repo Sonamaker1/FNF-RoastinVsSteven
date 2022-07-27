cfaa7d453a3226e51c113d989e7b1c790a0840b0
Bendeeeey2 <75838488+Bendeeeey2@users.noreply.github.com>
2022-07-27 16:24:58 +0300

    first

---
+++ source/grafex/states/substates/CNGameOverSubstate.hx
@@ -0,0 +1,23 @@
+package grafex.states.substates;
+
+import grafex.systems.Paths;
+import grafex.systems.Conductor;
+import grafex.systems.statesystem.MusicBeatState;
+import grafex.sprites.characters.Boyfriend;
+import grafex.systems.statesystem.MusicBeatSubstate;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.addons.display.FlxBackdrop;
+import flixel.FlxSubState;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+
+class CNGameOverSubstate extends MusicBeatSubstate
+{
+    var stripes:FlxBackdrop;
+    
+}
